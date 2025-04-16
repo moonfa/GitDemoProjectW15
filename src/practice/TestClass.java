9b23a28828257cb403d43ba71caee796c9f62b7b
moonfa <moonfa@msn.com>
2025-04-16 10:25:01 -0700

    second commit

--- src/practice/TestClass.java
+++ src/practice/TestClass.java
@@ -1,9 +1,14 @@
 package practice;
-
+//team--share project--
 public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Thhis is Git demo project");
+		System.out.println("create by team leader");
+		System.out.println("sss");
+		
+		//
 
 	}
 
